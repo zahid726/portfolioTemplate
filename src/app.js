a3266c4f57ccedc1542e721fe715e542ce13ca56
Zahid Hussain <56760279+zahid726@users.noreply.github.com>
2021-09-11 01:32:04 +0500

    vp

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('navList')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
+
