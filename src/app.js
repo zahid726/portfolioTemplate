ca69e5ce8eba379512bb27ab26b703808a197972
Zahid Hussain <56760279+zahid726@users.noreply.github.com>
2021-09-11 03:03:46 +0500

    1212

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('navList')
+// const hamburgerButton = document.getElementById('hamburger')
+// const navList = document.getElementById('navList')
 
-function toggleButton() {
-    navList.classList.toggle('show')
-}
+// function toggleButton() {
+//     navList.classList.toggle('show')
+// }
 
-hamburgerButton.addEventListener('click', toggleButton)
+// hamburgerButton.addEventListener('click', toggleButton)
 
+// const hamburger = document.querySelector(".hamburger");
+//     const navLinks = document.querySelector(".nav-links");
+//     const links = document.querySelectorAll(".nav-links li");
+
+//     hamburger.addEventListener("click", () => {
+//     navLinks.classList.toggle("open");
+//     links.forEach(link => {
+//         link.classList.toggle("fade");
+//     });
+//     });
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector("body");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("nav-links-open");
+
+});
